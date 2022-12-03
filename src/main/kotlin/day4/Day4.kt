d2539d2e8366ca60743bea01dbd841283f7da959
stcornis <steve.cornish@waitrose.co.uk>
2022-12-03 09:00:08 +0000

    New file ready for day 4

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,2 +1,27 @@
 package day4
 
+
+import readInput
+
+fun main() {
+
+    val day = "day4"
+    println("Output for $day")
+
+    fun part1(input: List<String>) : Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>) : Long {
+        TODO()
+    }
+
+    val testInput = readInput(day, "test.txt")
+    val input = readInput(day, "input.txt")
+
+    check(part1(testInput) == 0)
+//    println("Part 1 answer: ${part1(input)}")
+
+//    check(part2(testInput) == 0L)
+//    println("Part 2 answer: ${part2(input)}")
+}
